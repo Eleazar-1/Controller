522134a798908bb81cab719bf9329f240922ff11
Eleazar <elelopmah@alu.edu.gva.es>
2026-09-15 17:36:04 +0200

    Completar sesión 02: parámetros de ruta y de consulta

---
+++ README.md
@@ -0,0 +1,3 @@
+http://localhost:8080/reservas/resumen
+http://localhost:8080/estado
+http://localhost:8080/joan
---
+++ src/main/java/com/example/demo/HolaController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+    @GetMapping("/joan")
+    public String joan() {
+        return "Hola, mundo. Te responde mi servidor.";
+    }
+
+    @GetMapping("/estado")
+    public String estado() {
+        return "El servidor está funcionando.";
+    }
+
+    @GetMapping("/reservas/resumen")
+    public String resumen() {
+        return "La gestión será sobre prestamos.";
+    }
+
+    @GetMapping("/anyo")
+    public int anyo() {
+        return 2026;
+    }
+}
